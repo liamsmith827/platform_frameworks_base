18fa7b038b06d97c0b0b7808d5459a9e6977b820
liamsmith827 <liamsmith827@proton.me>
2026-08-18 00:40:13 +0100

    fixup! add helper class for launching non-standard Settings intents

--- core/java/android/ext/SettingsIntents.java
+++ core/java/android/ext/SettingsIntents.java
@@ -1,10 +1,13 @@
 package android.ext;
 
+import android.annotation.NonNull;
+import android.annotation.SystemApi;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 
 /** @hide */
+@SystemApi
 public class SettingsIntents {
 
     public static final String APP_NATIVE_DEBUGGING = "android.settings.OPEN_APP_NATIVE_DEBUGGING_SETTINGS";
@@ -13,12 +16,19 @@ public class SettingsIntents {
     public static final String APP_MEMORY_DYN_CODE_LOADING = "android.settings.OPEN_APP_MEMORY_DYN_CODE_LOADING_SETTINGS";
     public static final String APP_STORAGE_DYN_CODE_LOADING = "android.settings.OPEN_APP_STORAGE_DYN_CODE_LOADING_SETTINGS";
     public static final String APP_MANAGE_PLAY_INTEGRITY_API = "android.settings.OPEN_APP_MANAGE_PLAY_INTEGRITY_API_SETTINGS";
+    public static final String APP_STRICT_LEAK_BLOCKING = "android.settings.OPEN_APP_STRICT_LEAK_BLOCKING";
 
-    public static Intent getAppIntent(Context ctx, String action, String pkgName) {
+    // This constructor keeps the linter happy.
+    private SettingsIntents() {}
+
+    public static @NonNull Intent createAppIntent(@NonNull Context ctx, @NonNull String action,
+            @NonNull String pkgName, boolean newTask) {
         var i = new Intent(action);
         i.setData(Uri.fromParts("package", pkgName, null));
         i.setPackage(KnownSystemPackages.get(ctx).settings);
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        if (newTask) {
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        }
         return i;
     }
 }
